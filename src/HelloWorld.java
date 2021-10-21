31159be1231b6d877c746859886139fa3e50951b
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-10-21 16:13:42 -0500

    more practice

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,7 +6,8 @@ public class HelloWorld {
         System.out.println(" I'm going to take over Java.");
         System.out.print("Code inside of curly braces should be indented");
         System.out.print(" with four spaces.");
-        System.out.println(" Testing Java Syntax. This line of code should be attached to the string on line 8.");
-        System.out.print("Starting a new line.");
+        System.out.print(" Testing Java Syntax. This line of code should be attached to the string on line 8.");
+        System.out.println("Starting a new line.");
+        System.out.println("println tells the code where to stop reading and start a new line.");
     }
 }
