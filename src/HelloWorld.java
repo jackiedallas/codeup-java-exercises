debb9dc3a0f972160e4d52e55cd1d5febac7d427
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-10-21 15:55:29 -0500

    practing with java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.print("Hello, Jackie!");
+        System.out.println("Code inside of curly braces should be indented");
+        System.out.println("with four spaces");
     }
 }
