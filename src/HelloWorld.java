01d03cb4499b48f66cfc0a0af62702a4d8f13190
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-10-21 16:07:58 -0500

    completed exercise, played around with java print and println

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,12 @@
+import java.util.Arrays;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.print("Hello, Jackie!");
-        System.out.println("Code inside of curly braces should be indented");
-        System.out.println("with four spaces");
+        System.out.print("Hello, World!");
+        System.out.println(" I'm going to take over Java.");
+        System.out.print("Code inside of curly braces should be indented");
+        System.out.print(" with four spaces.");
+        System.out.println(" Testing Java Syntax. This line of code should be attached to the string on line 8.");
+        System.out.print("Starting a new line.");
     }
 }
