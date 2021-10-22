41c5c241c71766c480e92e2192f020d8415b0cfa
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-10-22 13:40:57 -0500

    used printf

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class ConsoleExercises {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         double pi = 3.14159;
+        String mathString = "PI is:";
+
+        System.out.printf("%s %s",mathString, pi);
     }
 }
