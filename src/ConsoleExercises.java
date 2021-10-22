c7e8924f631884823ccbc3c371eb320865e7b0fd
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-10-22 14:37:01 -0500

    experimenting with scanner possibilities

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -8,9 +8,14 @@ public class ConsoleExercises {
         String mathString = "PI is:";
         System.out.printf("%s %s \n",mathString, pi);
 
+        System.out.print("How old is your mom? ");
+        int userInput0 = scanner.nextInt();
         System.out.print("How old are you? ");
-        String userInput = scanner.nextLine();
-        System.out.printf("Wow, you've survived %s years on Earth. Congratulations!", userInput);
+        int userInput = scanner.nextInt();
+
+        System.out.printf("\nWow, you've survived %s years on Earth. Congratulations!", userInput);
+        System.out.printf("\nYour mom is %s years young!", userInput0);
+        System.out.print("\nYour mom was " + (userInput0 - userInput) + " when she gave birth to you.");
 
 
     }
