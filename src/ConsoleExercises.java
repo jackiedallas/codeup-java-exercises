64829e4ca681bc4c1fc619b662d4476ea40f3f4e
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-10-22 15:02:39 -0500

    converted pi to floating decimal to two places

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,12 +1,16 @@
 import java.util.Scanner;
 
+
 public class ConsoleExercises {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+
+
+
         double pi = 3.14159;
-        String mathString = "PI is:";
-        System.out.printf("%s %s \n",mathString, pi);
+        String mathString = "PI is approximately:";
+        System.out.format("%s %.2f%n \n",mathString, pi);
 
         System.out.print("How old is your mom? ");
         int userInput0 = scanner.nextInt();
