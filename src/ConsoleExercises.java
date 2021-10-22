0e3b507ead006de201b241783ca4c78ce7af9a2f
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-10-22 16:14:35 -0500

    finished scanner exercise

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -10,7 +10,7 @@ public class ConsoleExercises {
 
         double pi = 3.14159;
         String mathString = "PI is approximately:";
-        System.out.format("%s %.2f%n \n",mathString, pi);
+        System.out.format("%s %.2f%n",mathString, pi); // 'n' creates new line like "\n"
 
         System.out.print("How old is your mom? ");
         int userInput0 = scanner.nextInt();
@@ -20,6 +20,22 @@ public class ConsoleExercises {
         System.out.printf("\nWow, you've survived %s years on Earth. Congratulations!", userInput);
         System.out.printf("\nYour mom is %s years young!", userInput0);
         System.out.print("\nYour mom was " + (userInput0 - userInput) + " when she gave birth to you.");
+
+        System.out.println("\n------------------------------------");
+
+        System.out.print("What is your favorite number? ");
+        int userInput2 = scanner.nextInt();
+        System.out.printf("Your favorite number is %s.", userInput2);
+
+
+        scanner.nextLine();
+        System.out.print("\nEnter the 3 keywords (no punctuation necessary, just the words and a single space between them): ");
+
+        String userInputs = scanner.nextLine();
+//        String userInput3 = scanner.next();
+//        String userInput4 = scanner.next();
+//        String userInput5 = scanner.next();
+        System.out.printf("Correct, the keywords were: %s.",userInputs);
 
 
     }
