f6fa30e62321d142a1ea80ac563e532a2d49a609
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-10-22 22:53:37 -0500

    finished calculate perimeter and area

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -6,11 +6,9 @@ public class ConsoleExercises {
         Scanner scanner = new Scanner(System.in);
 
 
-
-
         double pi = 3.14159;
         String mathString = "PI is approximately:";
-        System.out.format("%s %.2f%n",mathString, pi); // 'n' creates new line like "\n"
+        System.out.format("%s %.2f%n", mathString, pi); // 'n' creates new line like "\n"
 
         System.out.print("How old is your mom? ");
         int userInput0 = scanner.nextInt();
@@ -35,8 +33,17 @@ public class ConsoleExercises {
 //        String userInput3 = scanner.next();
 //        String userInput4 = scanner.next();
 //        String userInput5 = scanner.next();
-        System.out.printf("Correct, the keywords were: %s.",userInputs);
+        System.out.printf("Correct, the keywords were: %s.", userInputs);
 
+        scanner.nextLine();
+        System.out.print("What is the length of the room your in? ");
+        int length = scanner.nextInt();
+        System.out.print("What's the width of room your in? ");
+        int width = scanner.nextInt();
+        int area = length * width;
+        int perimeter = 2 * (length + width);
+        System.out.printf("The area of your room is %s sqFt.", area);
+        System.out.printf("\nThe perimeter of your room is %s Ft.", perimeter);
 
     }
 }
