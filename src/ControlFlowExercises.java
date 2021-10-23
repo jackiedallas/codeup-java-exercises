863689c5f7ca2b4a68aae24e6f6bb40daa99a2d6
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-10-23 01:57:12 -0500

    completed count to 100 by 2s

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,9 +1,19 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 5;
-        while (i <= 15) {
-            System.out.println("Count is: " + i);
-            i++;
-        }
+
+        // 1. Loop Basics
+            // a. While
+//            int i = 5;
+//            while (i <= 15) {
+//                System.out.println("Count is: " + i);
+//                i++;
+//            }
+
+            // b. Do While
+            int i = 2;
+            do {
+                System.out.println("Count is: " + i);
+                i += 2;
+            } while (i <= 100);
     }
 }
