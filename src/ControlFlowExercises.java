00bd310530873c9d394a1c6888acec289cf04666
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-10-24 22:35:24 -0500

    completed fizzbuzz

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -42,8 +42,20 @@ public class ControlFlowExercises {
 //            System.out.println(i);
 //        }
 
-        for (long i = 2; i <= 1000000 ; i *= i) {
-            System.out.println(i);
+//        for (long i = 2; i <= 1000000 ; i *= i) {
+//            System.out.println(i);
+//        }
+
+        for (int i = 1; i <= 100 ; i++) {
+            if ((i % 3 == 0) && (i % 5 == 0)) {
+                System.out.println("Fizzbuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
         }
     }
 }
