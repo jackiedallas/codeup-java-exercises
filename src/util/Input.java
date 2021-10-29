175954bd3381c5a5d04829efb48f6cf42b8f8c79
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-10-29 01:07:38 -0500

    finished getInt and getDouble

--- src/util/Input.java
+++ src/util/Input.java
@@ -12,6 +12,7 @@ public class Input {
     public String getString() {
         return scanner.nextLine();
     }
+
     public boolean yesNo() {
 
         if (scanner.hasNext("y")) {
@@ -24,7 +25,32 @@ public class Input {
             return true;
         }
         return false;
+    }
 
+    public int getInt(int min, int max) {
+        int userNum;
+        do {
+            System.out.printf("Enter a number between %s and %s: ", min, max);
+            userNum = scanner.nextInt();
+        } while((userNum > max) || (userNum < min));
+        return userNum;
 
+    }
+
+    public int getInt() {
+        return scanner.nextInt();
+    }
+
+    public double getDouble(double min, double max) {
+        double userNum;
+        do {
+            System.out.printf("Enter a number between %s and %s: ", min, max);
+            userNum = scanner.nextDouble();
+        } while ((userNum > max) || (userNum < min));
+        return userNum;
+    }
+
+    public double getDouble() {
+        return scanner.nextDouble();
     }
 }
--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -13,9 +13,13 @@ public class InputTest {
 //        String userInput = input.getString();
 //        System.out.println("userInput = " + userInput);
 
-        System.out.print("10 < 5 [Y/N] ");
-        boolean userInput2 = input.yesNo();
-        System.out.println("userInput2 = " + userInput2);
+//        System.out.print("10 < 5 [Y/N] ");
+//        boolean userInput2 = input.yesNo();
+//        System.out.println("userInput2 = " + userInput2);
 
+//        System.out.println("input.getInt(1, 10) = " + input.getInt(1, 10));
+//        System.out.println("input.getDouble(1.00, 1.50) = " + input.getDouble(1.00, 1.50));
+        input.getDouble(1.00, 5.00);
+        System.out.println("input.getDouble() = " + input.getDouble());
     }
 }
