cf02c42acc0694f82f8d12d49f2114ffe7b2f8a0
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-11-05 11:18:11 -0500

    completed student class with add a grade and get an average, functionality working

--- src/Collections.java
+++ src/Collections.java
@@ -1,5 +1,7 @@
-import java.util.ArrayList;
-import java.util.Arrays;
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.HashMap;
+import java.util.*;
 
 public class Collections {
 
@@ -46,6 +48,51 @@ public class Collections {
         myFavoriteNums.remove(2);
         System.out.println();
         System.out.println(myFavoriteNums);
+
+
+        System.out.println("---------------- hash maps lecture below-----------------\n");
+
+        HashMap<String, String> usernames = new HashMap<>();
+        // inserting k : v pairs into hashmap
+        usernames.put("Jackie", "token");
+        usernames.put("Ian", "IansLame");
+        usernames.put("Adamina", "minaCold");
+
+        String jackieUserName = usernames.get("Jackie");
+
+        System.out.println("usernames = " + usernames);
+        System.out.println("usernames.get(\"Jackie\") = " + jackieUserName);
+
+        // oops we looked for nick as a key, no results found .. we end up with null
+        System.out.println("usernames.get(\"nick\") = " + usernames.get("nick"));
+
+        // providing a default might help
+        System.out.println("usernames.getOrDefault(\"Nick\", \"No User Found\") = " + usernames.getOrDefault("Nick", "No User Found"));
+
+        // .contains experimentation
+        System.out.println("usernames.containsKey(\"Jackie\") = " + usernames.containsKey("Jackie"));
+        System.out.println("usernames.containsValue(\"oken\") = " + usernames.containsValue("oken"));
+        System.out.println("usernames.containsValue(\"token\") = " + usernames.containsValue("token"));
+
+        System.out.println();
+
+        System.out.println(usernames);
+
+        usernames.putIfAbsent("Jackie", "goatCoder");
+        usernames.putIfAbsent("Nick", "reacter");
+
+        System.out.println(usernames);
+
+        System.out.println("usernames.getOrDefault(\"Nick\", \"User not found\") = " + usernames.getOrDefault("Nick", "User not found"));
+
+        usernames.remove("Nick");
+        System.out.println(usernames);
+        usernames.replace("Jackie", "goatCoder");
+        System.out.println(usernames);
+
+        usernames.clear();
+        System.out.println(usernames);
+        System.out.println("usernames.isEmpty() = " + usernames.isEmpty());
 
     }
 
--- src/Grades/Student.java
+++ src/Grades/Student.java
@@ -4,7 +4,44 @@ import java.util.ArrayList;
 
 public class Student {
 
+    // properties
     private String name;
-    private ArrayList<Integer> grades = new ArrayList<>();
+    private ArrayList<Integer> grades;
+
+    // constructors
+    public Student (String studentName, ArrayList<Integer> studentGrades) {
+        this.name = studentName;
+        this.grades = studentGrades;
+    }
+
+    // getters
+    public String getName() {
+        return this.name;
+    }
+    // add a grade
+    public void addGrade (int grade) {
+        grades.add(grade);
+    }
+    // returns average grade
+    public double getGradeAverage() {
+        double sumOfGrades = 0;
+        for (int i: grades) {
+            sumOfGrades += i;
+        }
+        return Math.round(sumOfGrades/grades.size());
+    }
+
+    public static void main(String[] args) {
+        Student jackie = new Student("Jackie", new ArrayList<>());
+
+        jackie.grades.add(89);
+        System.out.println(jackie.grades);
+        jackie.grades.add(88);
+        jackie.grades.add(74);
+        System.out.println("jackie.getGradeAverage() = " + jackie.getGradeAverage());
+    }
+
+
+
 
 }
