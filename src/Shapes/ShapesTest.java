96468d8e28cc8068ad50c59ef20b0aa6582835f4
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-11-04 22:18:37 -0500

    tested functionality with declaring a shape as a measurable

--- src/Shapes/ShapesTest.java
+++ src/Shapes/ShapesTest.java
@@ -3,26 +3,10 @@ package Shapes;
 public class ShapesTest {
 
 
-//    public static void doMath(Rectangle r) {
-//        System.out.println(r.getArea());
-//        System.out.println(r.getPerimeter());
-//    }
-//    public static void doSquareMath(Square s) {
-//        System.out.println(s.getArea());
-//        System.out.println(s.getPerimeter());
-//    }
-
     public static void main(String[] args) {
 
-//        Rectangle box1 = new Rectangle(5, 4);
-////        System.out.println("box1.getArea() = " + box1.getArea());
-////        System.out.println("box1.getPerimeter() = " + box1.getPerimeter());
-////        doMath(box1);
-//
-//        Square box2 = new Square(5);
-////        System.out.println("box2.getArea() = " + box2.getArea());
-////        System.out.println("box2.getPerimeter() = " + box2.getPerimeter());
-////        doSquareMath(box2);
+
+        Measurable myShape;
 
         Measurable mySquare = new Square(5);
         Measurable myRectangle = new Rectangle(5,5);
@@ -32,6 +16,11 @@ public class ShapesTest {
 
         System.out.println("myRectangle.getArea() = " + myRectangle.getArea());
         System.out.println("myRectangle.getPerimeter() = " + myRectangle.getPerimeter());
+
+        myShape = new Rectangle(5, 9);
+
+        System.out.println("myShape.getArea() = " + myShape.getArea());
+        System.out.println("myShape.getPerimeter() = " + myShape.getPerimeter());
 
 
     }
