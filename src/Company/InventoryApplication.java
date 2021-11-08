495c3c706e5942e1c80ae71d0c514423cb1fa8dd
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-11-08 01:08:30 -0600

    finished

--- src/Company/InventoryApplication.java
+++ src/Company/InventoryApplication.java
@@ -11,48 +11,45 @@ public class InventoryApplication {
     public static void main(String[] args) {
         ArrayList<Product> products = new ArrayList<>();
 
-
         Input input = new Input();
 
         System.out.println("Welcome to JD's Online Boutique!");
         System.out.println("Please add 4 new products.");
-        while(products.size() <= 3) {
-            System.out.println();
-            System.out.println("""
+
+            while(products.size() <= 3) {
+                System.out.println();
+                System.out.println("""
                 Please select a category for the product you'd like to add:\s
                 1. Car
                 2. Shoe
                 3. Subscription
                 4. Other""");
 
-            int userChoice = input.getInt();
-
-            switch (userChoice) {
-                case 1 -> {
-                    Car userCar;
-                    userCar = userAddCar();
-                    products.add(userCar);
+                int userChoice = input.getInt();
+                switch (userChoice) {
+                    case 1 -> {
+                        Car userCar;
+                        userCar = userAddCar();
+                        products.add(userCar);
+                    }
+                    case 2 -> {
+                        Shoe userShoe;
+                        userShoe = userShoe();
+                        products.add(userShoe);
+                    }
+                    case 3 -> {
+                        Subscription userSubscription;
+                        userSubscription = userSubscription();
+                        products.add(userSubscription);
+                    }
+                    case 4 -> {
+                        Product userProduct;
+                        userProduct = makeUserProduct();
+                        products.add(userProduct);
+                    }
+                    default -> System.out.println("Invalid option");
                 }
-                case 2 -> {
-                    Shoe userShoe;
-                    userShoe = userShoe();
-                    products.add(userShoe);
-                }
-                case 3 -> {
-                    Subscription userSubscription;
-                    userSubscription = userSubscription();
-                    products.add(userSubscription);
-                }
-                case 4 -> {
-                    Product userProduct;
-                    userProduct = makeUserProduct();
-                    products.add(userProduct);
-                }
-                default -> System.out.println("Invalid option");
             }
-
-        }
-
 
         System.out.println("Thanks for adding new products to our online store, here is a summary of the items you added!");
         System.out.println();
